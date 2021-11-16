77a6377d22adb7d4f28922a25d78782226cbf983
wlodekjankowski <93379724+wlodekjankowski@users.noreply.github.com>
2021-11-16 08:35:33 +0100

    Add files via upload

---
+++ klawisze strzałek i kolory/main.js
@@ -0,0 +1,41 @@
+let red = 100;
+let green = 100;
+let blue = 100;
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+const changeColor = (e) => {
+  // console.log(e.keyCode, e.which);
+  // 38 - strzałka do góry
+  // 40 - strzalka w dół
+
+  // INSTRUKCJA IF
+  // if (e.keyCode === 38 && red < 255) {
+
+  //   red += 1;
+  //   green += 1;
+  //   blue += 1;
+  //   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // } else if (e.keyCode === 40 && red >= 0) {
+  //   red--;
+  //   green--;
+  //   blue--;
+  //   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // }
+
+
+  // wersja 2 - instrukcja switch
+
+  switch (e.keyCode) {
+    case 38:
+      document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red}, ${green < 255 ? ++green : green}, ${blue < 255 ? ++blue : blue})`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red}, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
+      break;
+  }
+  console.log(red);
+
+}
+
+window.addEventListener('keydown', changeColor)
